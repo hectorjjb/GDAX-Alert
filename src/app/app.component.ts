2d2cc68b6552cff2adfd3e69a0cf7c8cf6a3325f
Hector Jimenez Blanco <hectorj@microsoft.com>
2018-01-18 18:56:37 -0800

    5 sec timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     title = 'app';
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         const exampleSocket = new WebSocket('wss://ws-feed.gdax.com');
         const msg: any = {
             'type': 'subscribe',
@@ -23,22 +23,46 @@ export class AppComponent implements OnInit {
             exampleSocket.send(JSON.stringify(msg));
 
         };
-        let count = 0;
-        exampleSocket.onmessage = function (event) {
+        let reset = false;
+        let buys: number[] = [];
+        let sells: number[] = [];
+        let b: number[] = [];
+        let s: number[] = [];
+        setInterval(() => reset = true, 5000);
+        exampleSocket.onmessage = (event) => {
             const data = JSON.parse(event.data);
             if (data && data.changes && data.changes[0][2] !== '0') {
+                const change = parseFloat(parseFloat(data.changes[0][1]).toFixed(2));
                 if (data.changes[0][0] === 'buy') {
-                    console.log(parseFloat(data.changes[0][1]).toFixed(2));
+                    // console.log(change);
+                    buys.push(change);
                 } else {
-                    console.log('                                                    ', parseFloat(data.changes[0][1]).toFixed(2));
+                    // console.log('                                                    ', change);
+                    sells.push(change);
                 }
-                count++;
-                if (count > 30) {
+                if (reset) {
                     console.clear();
-                    count = 0;
+                    b = this.GetStats('Buy', buys);
+                    s = this.GetStats('Sell', sells);
+                    console.log(`Vol : ${b[0] + s[0]}  avg: ${(b[0] * b[1] + s[0] * s[1]) / (b[0] + s[0])} mean: ${(b[2] + s[2]) / 2}`);
+                    buys = [];
+                    sells = [];
+                    reset = false;
                 }
             }
         };
     }
 
+    private GetStats(type: string, transactions: number[]): number[] {
+        let sum = 0;
+        transactions.sort();
+        for (const t of transactions) {
+            sum += t;
+        }
+        const total = transactions.length;
+        const avg = parseFloat((sum / total).toFixed(2));
+        const mean = parseFloat(transactions[Math.round(total / 2)].toFixed(2));
+        console.log(`${type} : ${total}  avg: ${avg} mean: ${mean}`);
+        return [total, avg, mean];
+    }
 }
