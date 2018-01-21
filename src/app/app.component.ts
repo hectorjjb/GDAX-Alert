af2b949dbccca553d8001265418905bd9566749a
Hector Jimenez <hectorjimenez@outlook.com>
2018-01-20 23:02:09 -0800

    new endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GdaxService } from './gdax.service';
+import { GdaxService, Tick } from './gdax.service';
 import { NotificationService } from './notification.service';
 
 
@@ -9,28 +9,18 @@ import { NotificationService } from './notification.service';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    public buyPrice = 0;
-    public buyVolume = 0;
-    public sellPrice = 0;
-    public sellVolume = 0;
-    public totalPrice = 0;
-    public totalVolume = 0;
-    private wantedPrice = 200;
+    public tick: Tick;
+    private wantedPrice = 205;
     private reset = false;
 
     constructor(private notificationSvc: NotificationService, private gdaxSvc: GdaxService) { }
 
     public ngOnInit(): void {
-        this.gdaxSvc.priceChange$.subscribe(changes => {
-            console.log(changes);
-            this.buyPrice = changes[0][1];
-            this.buyVolume = changes[0][0];
-            this.sellPrice = changes[1][1];
-            this.sellVolume = changes[1][0];
-            this.totalPrice =  changes[2][1];
-            this.totalVolume =  changes[2][0];
-            if (changes[2][1] > this.wantedPrice) {
-                this.notificationSvc.notify('$ ' + changes[2][1]);
+        this.gdaxSvc.priceChange$.subscribe(tick => {
+            console.log(tick);
+            this.tick = tick;
+            if (tick.price > this.wantedPrice) {
+                this.notificationSvc.notify('$ ' + tick.price);
             }
         });
     }
--- src/app/gdax.service.ts
+++ src/app/gdax.service.ts
@@ -5,62 +5,47 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class GdaxService {
-    public priceChange$: Observable<number[][]>;
-    private changes = new ReplaySubject<number[][]>(1);
-    private reset = false;
+    public priceChange$: Observable<Tick>;
+    private changes = new ReplaySubject<Tick>(1);
     private socket = new WebSocket('wss://ws-feed.gdax.com');
-    private buys: number[] = [];
-    private sells: number[] = [];
-    private b: number[] = [];
-    private s: number[] = [];
-    private SUM = (a, b) => a + b;
 
     constructor() {
         this.priceChange$ = this.changes.asObservable();
         this.socket = new WebSocket('wss://ws-feed.gdax.com');
         const msg: any = {
             'type': 'subscribe',
-            'product_ids': [
-                'LTC-USD',
-            ],
-            'channels': [
-                'level2'
-            ]
+            'channels': [{ 'name': 'ticker', 'product_ids': ['LTC-USD'] }]
         };
         this.socket.onopen = (event) => {
             this.socket.send(JSON.stringify(msg));
         };
-        setInterval(() => this.reset = true, 5000);
         this.socket.onmessage = (event) => {
             const data = JSON.parse(event.data);
-            if (data && data.changes && data.changes[0][2] !== '0') {
-                const change = parseFloat(parseFloat(data.changes[0][1]).toFixed(2));
-                if (data.changes[0][0] === 'buy') {
-                    this.buys.push(change);
-                } else {
-                    this.sells.push(change);
-                }
-                if (this.reset) {
-                    console.clear();
-                    this.b = this.GetStats(this.buys);
-                    this.s = this.GetStats(this.sells);
-                    const p = parseFloat((((this.b[0] * this.b[1]) + (this.s[0] * this.s[1])) / (this.b[0] + this.s[0])).toFixed(2));
-                    // console.log(`Vol : ${this.b[0] + this.s[0]}  avg: ${p}`);
-                    this.buys = [];
-                    this.sells = [];
-                    this.reset = false;
-                    this.changes.next([this.b, this.s, [this.b[0] + this.s[0], p]]);
-                }
+            if (data.type === 'ticker') {
+                data.price = parseFloat(data.price);
+                data.low_24h = data.low_24h.replace(/0+$/, '');
+                data.high_24h = data.high_24h.replace(/0+$/, '');
+                data.open_24h = data.open_24h.replace(/0+$/, '');
+                this.changes.next(data);
             }
         };
     }
+}
 
-    private GetStats(transactions: number[]): number[] {
-        const sum = transactions.reduce(this.SUM);
-        const total = transactions.length;
-        const avg = parseFloat((sum / total).toFixed(2));
-        // console.log(`Total: ${total}  avg: ${avg}`);
-        return [total, avg];
-    }
-
+export class Tick {
+    public best_ask: string;
+    public best_bid: string;
+    public high_24h: string;
+    public last_size: string;
+    public low_24h: string;
+    public open_24h: string;
+    public price: number;
+    public product_id: string;
+    public sequence: number;
+    public side: string;
+    public time: string;
+    public trade_id: number;
+    public type: string;
+    public volume_24h: string;
+    public volume_30d: string;
 }
